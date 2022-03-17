2006477d5dae114290773a7ae7a0f60db736cd9f
Anubhav <chauhananubhav0011@gmail.com>
2022-03-17 23:21:43 +0530

    literals done

---
+++ src/com/company/Literals.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Literals {
+    public static void main(String[] args){
+        byte age = 20;
+        char ch='A';
+        float F = 4.5f;
+        long num = 333333311298397L;
+        double D = 4.55d;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(F);
+        System.out.println(D);
+        System.out.println(num);
+        System.out.print(a);
+
+        String str = "Abc";
+        System.out.println(str);
+    }
+}
