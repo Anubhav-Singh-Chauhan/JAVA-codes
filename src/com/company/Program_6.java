e9a30efef6652675a1e1f362f58b6a6ef67f5824
Anubhav <chauhananubhav0011@gmail.com>
2022-03-18 15:23:20 +0530

    code 6

---
+++ src/com/company/Program_6.java
@@ -0,0 +1,20 @@
+package com.company;
+//1. replace spaces with underscores in a string.
+//2. Detect triple and double spaces in a string.
+public class Program_6 {
+    public static void main(String[] args) {
+// 1.
+        String str = "Hello good morning";
+        System.out.println(str);
+        System.out.println("Now we are replacing spaces with underscores, so string becomes");
+        System.out.println(str.replace(' ','_'));
+// 2.
+        String s1 ="This  is a   string";
+        System.out.print("Double spaces is at index: ");
+        System.out.println(s1.indexOf("  "));
+        System.out.print("Triple spaces is at index: ");
+        System.out.println(s1.indexOf("   "));
+
+
+    }
+}
