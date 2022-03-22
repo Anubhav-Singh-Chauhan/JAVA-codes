eca2ade27fa32fa98cf0bbf25746078e1bbd9588
Anubhav <chauhananubhav0011@gmail.com>
2022-03-22 23:03:51 +0530

    code 7

--- src/com/company/C7_PracticeProblem_5.java
+++ src/com/company/C7_PracticeProblem_5.java
@@ -1,6 +1,6 @@
 package com.company;
 import java.util.Scanner;
-public class Program_7 {
+public class C7_PracticeProblem_5 {
     public static void main(String[] args) {
         System.out.print("Enter a String: ");
         Scanner sc = new Scanner(System.in);
