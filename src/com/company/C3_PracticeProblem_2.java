af6e45c7456eebd93ad60dc854b4ee9f584eb61d
Anubhav <chauhananubhav0011@gmail.com>
2022-03-22 23:02:32 +0530

    code 3

--- src/com/company/C3_PracticeProblem_2.java
+++ src/com/company/C3_PracticeProblem_2.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 //program to check whether a number entered is integer or not
-public class Program_3 {
+public class C3_PracticeProblem_2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.hasNextInt());
