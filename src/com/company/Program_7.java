0cd39e97986e56ca662afa063586da02dddde631
Anubhav <chauhananubhav0011@gmail.com>
2022-03-22 21:42:07 +0530

    code 7

---
+++ src/com/company/Program_7.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Program_7 {
+    public static void main(String[] args) {
+        System.out.print("Enter a String: ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String s=str.toLowerCase();
+
+        System.out.println(s);
+
+    }
+}
