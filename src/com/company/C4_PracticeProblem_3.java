5d1b07fbadd471d3909d29f43c0800a9321f4b7e
Anubhav <chauhananubhav0011@gmail.com>
2022-03-22 23:02:51 +0530

    code 4

--- src/com/company/C4_PracticeProblem_3.java
+++ src/com/company/C4_PracticeProblem_3.java
@@ -1,6 +1,6 @@
 package com.company;
 import java.util.Scanner;
-public class Program_4 {
+public class C4_PracticeProblem_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
      //   String str = sc.next();
