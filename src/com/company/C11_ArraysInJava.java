b0a0dc5d929b03aed3f50b32966e8f06cdffae9b
Anubhav <chauhananubhav0011@gmail.com>
2022-03-25 20:47:44 +0530

    code 10

---
+++ src/com/company/C11_ArraysInJava.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class C11_ArraysInJava {
+    public static void main(String[] args) {
+        // 1. Declaration and Memory Allocation at same time
+        int [] arr = new int[5];
+        arr[0]=2;
+        arr[1]=3;
+        arr[2]=5;
+        arr[3]=12;
+        arr[4]=13;
+
+        // 2. Declaration and then Memory Allocation.
+        int [] A;
+        A = new int[2];
+        A[0]=5;
+        A[1]=2;
+
+        // 3. Declaration, Memory Allocation and Initialisation same time.
+        int [] Arr = {2,3,4,5};
+
+
+    }
+}
