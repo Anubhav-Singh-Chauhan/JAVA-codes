84dbb4a07dcd61223d4f18b0086a178ef7bda002
Anubhav <chauhananubhav0011@gmail.com>
2022-03-25 20:21:23 +0530

    code 10

---
+++ src/com/company/C10_PracticeProblem_7.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class C10_PracticeProblem_7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=0;i<=10;i++){
+            System.out.printf("%d x %d = %d\n",n,i,n*i);
+            sum = sum + (n*i);
+        }
+        System.out.printf("Sum of Results is: %d\n ",sum);
+    }
+}
