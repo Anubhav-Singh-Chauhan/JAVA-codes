c4aa5323bc6ab1a589f90e4c11080d4b46ceb8f3
Anubhav <chauhananubhav0011@gmail.com>
2022-03-25 21:00:55 +0530

    code 12

---
+++ src/com/company/C12_OperationInArrays.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class C12_OperationInArrays {
+    public static void main(String[] args) {
+        float [] arr={1.1f,23.5f,11.3f,12.0f,10.10f,5.5f,6.9f};
+        System.out.println("length of array is:" + arr.length);
+
+//        for(int i=0;i<arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
+
+        //Printing using For-each loop
+        for (float v : arr) {
+            System.out.println(v);
+        }
+
+    }
+}
