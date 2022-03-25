f2bcbb0679445f423b391b2e7f8099e3ac724645
Anubhav <chauhananubhav0011@gmail.com>
2022-03-25 21:24:03 +0530

    code 13

--- src/com/company/C13_2dArrays.java
+++ src/com/company/C13_2dArrays.java
@@ -2,13 +2,7 @@ package com.company;
 
 public class C13_2dArrays {
     public static void main(String[] args) {
-        int [][] arr = new int [2][3];
-        arr[0][0]=10;
-        arr[0][1]=11;
-        arr[0][2]=12;
-        arr[1][0]=13;
-        arr[1][1]=14;
-        arr[1][2]=15;
+        int [][] arr = {{10,11,12},{13,14,15}};
 
 //        for(int i=0;i<arr.length;i++){
 //            for(int j=0;j<arr[i].length;j++){
