4491649e4279852ae116a111da4a28d23524dac4
Anubhav <chauhananubhav0011@gmail.com>
2022-03-28 19:29:35 +0530

    code 10

--- src/com/company/Code10_PracticeProblem_7.java
+++ src/com/company/Code10_PracticeProblem_7.java
@@ -1,6 +1,6 @@
 package com.company;
 import java.util.Scanner;
-public class C10_PracticeProblem_7 {
+public class Code10_PracticeProblem_7 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
