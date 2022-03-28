fe08de360560628fa9e2822eb755f74a8df9d79a
Anubhav <chauhananubhav0011@gmail.com>
2022-03-28 19:30:04 +0530

    code 12

--- src/com/company/Code12_OperationInArrays.java
+++ src/com/company/Code12_OperationInArrays.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class C12_OperationInArrays {
+public class Code12_OperationInArrays {
     public static void main(String[] args) {
         float [] arr={1.1f,23.5f,11.3f,12.0f,10.10f,5.5f,6.9f};
         System.out.println("length of array is:" + arr.length);
