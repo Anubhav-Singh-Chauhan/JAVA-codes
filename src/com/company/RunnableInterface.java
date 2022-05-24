dddca8a43878c7f1f1d00eeb213bc7cf4d1ef21c
Anubhav <chauhananubhav0011@gmail.com>
2022-05-24 23:38:41 +0530

    Thread_Create

---
+++ src/com/company/RunnableInterface.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.lang.*;
+//Runnable Interface - This the better way to create thread.
+public class RunnableInterface implements Runnable
+{
+    public void run()
+    {
+        System.out.println("Thread Task");
+    }
+    public static void main(String[] args)
+    {
+        RunnableInterface t = new RunnableInterface();
+        //Create an object of Thread Class and pass the parameter in the constructor.
+        Thread th = new Thread(t);
+        th.start();
+    }
+}
