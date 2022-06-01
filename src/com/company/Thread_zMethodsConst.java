98f8fe5173deea3931efdb1746435e4d303003b8
Anubhav <chauhananubhav0011@gmail.com>
2022-06-01 15:17:36 +0530

    Type Of Methods & Constructors

---
+++ src/com/company/Thread_zMethodsConst.java
@@ -0,0 +1,58 @@
+package com.company;
+
+/*
+CONSTRUCTORS:
+1.Thread(){} -> Default Constructor
+2.Thread(Runnable Target){}
+3.Thread(String name){}
+4.Thread(Runnable Target,String name){}
+5.Thread(ThreadGroup tg,Runnable target){}
+6.Thread(ThreadGroup tg,String name){}
+7.Thread(ThreadGroup tg,Runnable Target,String name){}
+8.Thread(ThreadGroup tg,Runnable Target,String name,long stackSize){}
+
+METHODS:
+Basic Methods -
+1.run()
+2.start()
+3.currentThread()
+4.isAlive()
+
+Related to name -
+1.getName()
+2.setName(String name)
+
+Daemon Thread Related methods -
+1.isDaemon()
+2.setDaemon(boolean b)
+
+Priority Related methods -
+1.getPriority()
+2.setPriority()
+
+Preventing Thread Execution methods -
+1. sleep()
+2.yield()
+3.join()
+4.suspend() (deprecated,not in use)
+5.resume()  (deprecated)
+6.destroy() (deprecated)
+7.stop()    (deprecated)
+
+Thread Interruption Related Methods -
+1.interrupt()
+2.isInterrupted()
+3.interrupted()
+
+Inter-Thread Communication (Not come under thread class)
+1.wait()      [Comes under object class]
+2.notify()    [Comes under object class]
+3.notifyAll()   [Comes under object class]
+
+ */
+
+public class Thread_zMethodsConst {
+    public static void main(String[] args) {
+
+    }
+}
