faa0abcdfc98de55722eda5bfae7715e5a7138b2
Anubhav <chauhananubhav0011@gmail.com>
2022-07-04 22:08:56 +0530

    swing

--- src/com/company/SwingInJava.java
+++ src/com/company/SwingInJava.java
@@ -19,6 +19,25 @@ public class SwingInJava {
         JButton jb = new JButton("Click Here");
         jb.setBounds(200,150,100,50);
         jf.add(jb);
+
+
+        String Subject[] = {"Maths", "Hindi", "English", "Science"};
+        JList list = new JList(Subject);
+        list.setBounds(100,200,100,70);
+        jf.add(list);
+
+        String Data[][] = {{"Maths","100"}, {"Hindi","80"}, {"English","90"}, {"Science","96"},{"SST","60"},{"Java","100"}};
+        String Column[] = {"Subject","Marks"};
+
+        JTable table = new JTable(Data,Column);
+        table.setBounds(100,300,200,100);
+        jf.add(table);
+
+//        JScrollBar sb = new JScrollBar();
+//        sb.setBounds(520,20,80,400);
+//        jf.add(sb);
+        JScrollPane sp = new JScrollPane(table);
+        jf.add(sp);
         jf.setVisible(true);
 
     }
