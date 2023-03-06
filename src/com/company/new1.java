14443340882a76b13093468d98162d852c6bff28
*Anubhav <91267721+Anubhav-Singh-Chauhan@users.noreply.github.com>
2023-03-07 00:34:04 +0530

    javaPrac

---
+++ src/com/company/new1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class new1 {
+    public static void main(String[] args) {
+        int a;
+        System.out.println("Enter: ");
+        Scanner s = new Scanner(System.in);
+        a=s.nextInt();
+        System.out.println(a);
+    }
+}
