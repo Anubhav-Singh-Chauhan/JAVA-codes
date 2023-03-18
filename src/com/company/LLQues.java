d1f5256dae9d2675c02a5e8e5205cf01fc446669
*Anubhav <91267721+Anubhav-Singh-Chauhan@users.noreply.github.com>
2023-03-19 00:18:38 +0530

    LL_ques

--- src/com/company/LLQues.java
+++ src/com/company/LLQues.java
@@ -1,6 +1,6 @@
 public class LLQues {
 
-    // Q1 Reverse a Linked List 
+//        Q1 Reverse a Linked List 
 /* 
 
     public void reverse()
@@ -22,4 +22,87 @@ public class LLQues {
 */
 
 
+//         Q2 Find and remove Nth node from End
+
+/*
+    public void deleteNthFromEnd(int n)
+    {
+        int Size = 0;
+        Node temp = head;
+        while(temp!=null)
+        {
+            temp = temp.next;
+            Size++;
+        }
+
+        if(n==Size)
+        {
+            head = head.next;
+            return;
+        }
+        int i=1;
+        int iToFind = Size - n;
+        Node prev = head;
+        while(i<iToFind)
+        {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+ */
+
+
+ //         Q3 check LL is plaindrome or not
+ /*
+  public Node findMid(Node head)
+  {
+      Node slow = head;
+      Node fast = head;
+
+      while(fast != null && fast.next!=null)
+      {
+          slow = slow.next;
+          fast = fast.next.next;
+      }
+      return slow;
+  }
+
+  public boolean isPalindrome()
+  {
+      if(head==null || head.next==null){
+          return true;
+      }
+      //find mid
+      Node mid = findMid(head);
+
+      //reverse 2nd half
+      Node prev = null;
+      Node curr = mid;
+      Node next;
+      while(curr!=null)
+      {
+          next = curr.next;
+          curr.next = prev;
+          prev = curr;
+          curr = next;
+      }
+      Node right = prev;
+      Node left = head;
+
+      //check left half == right half
+
+      while(right != null )
+      {
+          if(left.data != right.data){
+              return false;
+          }
+          left = left.next;
+          right = right.next;
+      }
+
+      return true;
+  }
+  */
 }
