0d9a572779c84e8bbbaad8ec111b9190b4940941
*Anubhav <91267721+Anubhav-Singh-Chauhan@users.noreply.github.com>
2023-03-22 00:04:49 +0530

    LLQues

--- src/com/company/LLQues.java
+++ src/com/company/LLQues.java
@@ -178,4 +178,104 @@ public class LLQues {
      * 
      * }
      */
+
+
+     // Q6 Mergesort in a Linked List
+     /*
+      public Node findMid()
+    {
+        Node left = head;
+        Node right = head.next;
+        while(right!=null && right.next!=null)
+        {
+            right = right.next;
+            left = left.next;
+        }
+        return left;
+    }
+    public Node merge(Node l1, Node l2)
+    {
+        Node mergeNode = new Node(-1);
+        Node temp = mergeNode;
+        while(l1!=null && l2!=null)
+        {
+            if(l1.data <= l2.data){
+                temp.next = l1;
+                l1 = l1.next;
+                temp = temp.next;
+            }else{
+                temp.next = l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
+        }
+        while(l1!=null)
+        {
+            temp.next = l1;
+            l1 = l1.next;
+            temp = temp.next;
+        }
+
+        while(l2!=null)
+        {
+            temp.next = l2;
+            l2 = l2.next;
+            temp = temp.next;
+        }
+
+        return mergeNode.next;
+    }
+
+    public Node mergeSort(Node head)
+    {
+        if(head == null || head.next==null)
+        {
+            return head;
+        }
+
+        //find Mid
+        Node mid = findMid();
+
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newRight = mergeSort(rightHead);
+        Node newLeft = mergeSort(head);
+        return merge(newLeft,newRight);
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        //add
+        ll.addFirst(1);
+        ll.addFirst(3); 
+        ll.addLast(4);
+        ll.addLast(6);
+        ll.addLast(2);
+
+        // ll.addAtIndex(2,9);
+        ll.print();
+        ll.head = ll.mergeSort(head);
+
+        // //size
+        // System.out.println(size);
+
+        //remove
+        // ll.removeFirst();
+        ll.print();
+        // ll.removeLast();
+        // ll.print();
+
+        //search
+        // ll.itrSearch(9);
+        // head = new Node(1);
+        // Node temp = new Node(2);
+        // head.next = temp;
+        // head.next.next = new Node(3);
+        // head.next.next.next = temp;
+
+        // RemoveCycle();
+        // System.out.println(RemoveCycle());
+
+    }
+      */
 }
