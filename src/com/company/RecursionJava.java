29a4622cf1fe208360db9057e77fcd1ff1058cdd
*Anubhav <91267721+Anubhav-Singh-Chauhan@users.noreply.github.com>
2023-03-31 22:14:09 +0530

    Update RecursionJava.java

--- src/com/company/RecursionJava.java
+++ src/com/company/RecursionJava.java
@@ -1,17 +1,31 @@
 public class RecursionJava {
 
+    //print value in decreasing order using recursion
     public static void printValDec(int n)
     {
         if(n==1){
             System.out.println(n);
             return;
         }
-        System.out.println(n+" ");
+        System.out.print(n+" ");
         printValDec(n-1);
+    }
+
+    //print value in increasing order using recursion
+    public static void printValInc(int n)
+    {
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printValInc(n-1);
+        System.out.print(n+" ");
+        
     }
     public static void main(String[] args) {
         int n = 10;
         printValDec(n);
+        printValInc(n);
     }
     
 }
